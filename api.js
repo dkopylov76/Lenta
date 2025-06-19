933699be0f2e4f135f0ad61ad5803b1b0e8d29d9
dkopylov76 <d.kopylov.76@gmail.com>
2025-06-19 20:59:36 +0300

    api_3

--- api.js
+++ api.js
@@ -1,10 +1,22 @@
+import { comments } from "./comments"
+
 const server = "https://wedev-api.sky.pro/api/v1/dkopylov"
 
 export const fetchComments = () => {
     return fetch(server + "/comments").then((response) => {
         return response.json()
     })
-    .then((data) => {
-        console.log(data);
+    .then((responseData) => {
+        const appComments = responseData.comments.map(comment => {
+            return {
+                name: comment.author.name,
+                date: new Date(comment.date),
+                text: comment.text,
+                likes: comment.likes,
+                isLiked: false,
+            }
+        })
+
+        return appComments;
     })
 }
