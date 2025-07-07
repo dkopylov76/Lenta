6b564b57dd70463d0543c3cec5135fad6eb5e1ea
dkopylov76 <d.kopylov.76@gmail.com>
2025-07-08 00:07:49 +0300

    auth_23

--- renderRegistration.js
+++ renderRegistration.js
@@ -1,13 +1,20 @@
-import { login, setToken, setName, fetchComments } from "./api.js"
+import { setToken, setName, registration } from "./api.js"
 import { fetchAndRenderComments } from "./index.js";
-import { renderRegistration } from "./renderRegistration.js";
+import { renderLogin } from "./renderLogin.js";
 
-export const renderLogin = () => {
+export const renderRegistration = () => {
     const container = document.querySelector(".container")
 
     const loginHtml = `
         <section class="add-form">
-            <h1>Форма входа</h1>
+            <h1>Форма регистрации</h1>
+            <input
+                type="text"
+                class="add-form-name"
+                placeholder="Введите имя"
+                id="name"
+                required
+            />
             <input
                 type="text"
                 class="add-form-name"
@@ -23,24 +30,25 @@ export const renderLogin = () => {
                 required
             ></input>
             <fieldset class="add-form-registry">
-                <button class="add-form-button-main button-main" type="submit">Войти</button>
-                <u class="add-form-button-link registry">Зарегистрироваться</u>
+                <button class="add-form-button-main button-main" type="submit">Зарегистрироваться</button>
+                <u class="add-form-button-link entry">Войти</u>
             </fieldset>
         </section>
     `
 
     container.innerHTML = loginHtml;
 
-    document.querySelector(".registry").addEventListener("click", () => {
-        renderRegistration()
+    document.querySelector(".entry").addEventListener("click", () => {
+        renderLogin()
     })
 
+    const nameEl = document.querySelector("#name")
     const loginEl = document.querySelector("#login")
     const passwordEl = document.querySelector("#password")
-    const submitButtonEl = document.querySelector(".add-form-button-main")
+    const submitButtonEl = document.querySelector(".button-main")
 
     submitButtonEl.addEventListener("click", () => {
-        login(loginEl.value, passwordEl.value)
+        registration(nameEl.value, loginEl.value, passwordEl.value)
         .then((response) => {
             return response.json()
         })
